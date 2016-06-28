8cc995a6c02a37014281b0bb44a0a72bb4345112
Matthias Baumgartner <dev@igsor.net>
2016-06-28 18:10:39 +0200

    distutils support

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='bibdl',
+    version='0.1',
+    description='A tool to parse a bibliography and download the PDFs',
+    long_description=open('README.md').read(),
+    author='Matthias Baumgartner',
+    author_email='dev@igsor.net',
+    license='Free for use',
+)
+
+## EOF ##
